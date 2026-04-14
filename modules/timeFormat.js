9d8c8dbed71626709a16b4af39b37141fc29d6e5
DTH235644-LENHUTHAO <lenhuthao4812@gmail.com>
2026-04-14 08:12:28 +0700

    Upload_cuoi

--- modules/timeFormat.js
+++ modules/timeFormat.js
@@ -6,6 +6,9 @@ function formatTime(date) {
     const minutes = Math.floor(seconds / 60);
     const hours = Math.floor(minutes / 60);
     const days = Math.floor(hours / 24);
+    const weeks = Math.floor(days / 7);
+    const months = Math.floor(days / 30);
+    const years = Math.floor(days / 365);
 
     let timeAgo = "";
 
@@ -13,7 +16,9 @@ function formatTime(date) {
     else if (minutes < 60) timeAgo = minutes + " phút trước";
     else if (hours < 24) timeAgo = hours + " giờ trước";
     else if (days < 7) timeAgo = days + " ngày trước";
-    else timeAgo = d.toLocaleDateString('vi-VN');
+    else if (weeks < 4) timeAgo = weeks + " tuần trước";
+    else if (months < 12) timeAgo = months + " tháng trước";
+    else timeAgo = years + " năm trước";
 
     const fullDate = d.toLocaleDateString('vi-VN');
 
